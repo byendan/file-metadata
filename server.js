3f4f64090db91fbb89f7b9ac974e80858c20df97
Brendan <byendanb@gmail.com>
2016-12-09 23:43:36 +0000

    File uploads, and can see size

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 var express = require('express')
 var app = express()
+var multer  = require('multer')
+var storage = multer.memoryStorage()
+var upload = multer({ storage: storage })
+var type = upload.single('temp')
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/',function(req,res){
+      res.sendFile(__dirname + "/views/index.html");
+});
+
+app.post('/api/get-byte-size', function(req, res) {
+    type(req,res,function(err) {
+        if(err) throw err 
+        var return_size = {
+            "size": req.file.size
+        }
+        res.send(return_size);
+    });
 })
 
 app.listen((process.env.PORT || 8080), function () {
